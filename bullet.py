31277d0dd853a29d1d62d53de32bc0322b1b9526
chu-t <t.chu121199@gmail.com>
2017-12-05 16:54:30 -0500

    Add files via upload

--- bullet.py
+++ bullet.py
@@ -4,11 +4,16 @@ from pygame.locals import *
 class Bullet(pygame.sprite.Sprite):
     """Class creates bullet"""
     def __init__(self,pos):
+        """
+        Descr: Function creates bullet and initial positions
+        Params: pos: tuple containing coordinates of invader
+        Returns: none """
+        
         #initialize all the Sprite functionality
         pygame.sprite.Sprite.__init__(self)
         #Creating image and rect
-        self.image = pygame.image.load('assets/' + "bullet.png").convert()
-        self.image = pygame.transform.scale(self.image, (10,10))
+        self.image = pygame.image.load('assets/' + "bullet.png").convert_alpha()
+        self.image = pygame.transform.scale(self.image, (20,20))
         self.rect = self.image.get_rect()
         #Initial position
         self.rect.x = pos[0]
@@ -16,18 +21,14 @@ class Bullet(pygame.sprite.Sprite):
 
             
     def to_invader(self,enemypos):
-        """Moving position to enemy position"""
-        #Finds distance to invader
-        dist = ((enemypos[0]-self.rect.x)**2)+((enemypos[1]-self.rect.y)**2)**0.5
-        if (dist != 0):
-            if enemypos[0] > self.rect.x:
-                self.rect.x -= (enemypos[0] + 30)
-            elif enemypos[0] < self.rect.x:
-                self.rect.x += (enemypos[0] + 30)
-            elif enemypos[1] > self.rect.x:
-                self.rect.y -= (enemypos[1]+ 30)
-            elif enemypos[1] < self.rect.x:
-                self.rect.y += (enemypos[1]+ 30)
+        """
+        Descr:Function moves bullet to position of invader
+        Params: enemypos: tuple containing invader position
+        Returns: none """
+        
+        self.rect.x = enemypos[0]
+        self.rect.y = enemypos[1]
+
 
 
 
