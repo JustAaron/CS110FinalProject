ff43258ab2368da94b2b94043f38abb83c6a19d4
chu-t <t.chu121199@gmail.com>
2017-12-05 17:06:25 -0500

    Update tower.py

--- tower.py
+++ tower.py
@@ -6,6 +6,12 @@ class Tower(pygame.sprite.Sprite):
     """Class represents the towers that protects the castle by killling invaders"""
     
     def __init__(self, mouse, img_file = "tower.png",radius=100,price=100):
+        """
+        Descr: __init__ sets initial variables and image
+        Params: mouse is a tuple containing coordinates
+                img_file is a string containing image name
+                radius and price is int 
+        Returns: none """
     	#initialize all the Sprite functionality
     	pygame.sprite.Sprite.__init__(self)
     	
@@ -21,6 +27,10 @@ class Tower(pygame.sprite.Sprite):
     	self.rect.y = mouse[1]
     	
     def followmouse(self, change):
+        """
+        Descr: followmouse changes rect based on change 
+        Params: change is a tuple containing new coordinates of change
+        Returns: none """
     	self.rect.x += change[0]
     	self.rect.y += change[1]
     	
@@ -33,15 +43,17 @@ class Tower(pygame.sprite.Sprite):
     		self.image = pygame.image.load('assets/' + img_file).convert_alpha()
     		self.rect.x = rect[0]
     		self.rect.y = rect[1]
-    	else:
-    		return "Unable to upgrade: Money is not enough"
     		
     def inRange(self,enemyX,enemyY):
+        """
+        Descr: inRange tells if enemy is in range of tower
+        Params: enemyX/enemyY are int of the x and y coordinates
+        Returns: Boolean Expression(True/False) if in/not in range """
     	distanceToEnemy = math.sqrt((enemyX - self.rect.x)**2 + (enemyY - self.rect.y)**2)
     	if (distanceToEnemy < self.radius):
     		return True
-        else:
-            return False
+    	else:
+    		return False
     		
     def get_info(self):
     	self.info = ""
